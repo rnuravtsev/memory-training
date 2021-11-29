d7c64056b0a9ec22df6223ad9cc708c3d0bddf58
rnuravtsev <muravtsev21@gmail.com>
2021-11-29 23:15:53 +0300

    debounce в react?

--- src/components/GameFooter/GameFooter.jsx
+++ src/components/GameFooter/GameFooter.jsx
@@ -8,9 +8,22 @@ const GameFooter = () => {
     const onButtonClick = () => {
         dispatch(startGame())
     }
+
+    const onGameButtonMouseMove = () => {
+        console.log('Mouse move')
+    }
+
+    const debounce = (cb, time = 1000) => {
+        let timeout
+        return () => {
+            clearTimeout(timeout)
+            timeout = setTimeout(() => cb(), time)
+        }
+    }
+
     return (
         <div className="game-footer">
-            <button onClick={onButtonClick} className="game-footer__button" type="button">Начать</button>
+            <button onClick={onButtonClick} onMouseMove={debounce(onGameButtonMouseMove)} className="game-footer__button" type="button">Начать</button>
         </div>
     )
 };
